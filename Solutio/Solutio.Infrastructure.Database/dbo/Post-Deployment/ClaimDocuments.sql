856aeee587c1467315b1421b4bfc0497275ecc04
mpadini-eng <56047742+mpadini-eng@users.noreply.github.com>
2019-10-22 15:44:11 -0300

    new file

--- Solutio/Solutio.Infrastructure.Database/dbo/Post-Deployment/ClaimDocuments.sql
+++ Solutio/Solutio.Infrastructure.Database/dbo/Post-Deployment/ClaimDocuments.sql
@@ -9,22 +9,35 @@ USING (VALUES
 <head>
 	<style>
 		.container{
-			    width: 747px;
-			height: 855px;
+			width: 1120;
 			padding: 60px;
-			font-size: 16px;
+			font-size: 18px;
+			font-family: sans-serif;
 		}
 		
 		.caja{
-			width: 400px;
-			margin-left: 130px;
+			width: 546px;
+			margin-left: 276px;
 			padding: 17px;
 			text-align: left;
-			border: 1px solid black;
+			border: 1px solid #454666;
+		}
+		
+		.barraSup{
+			background-color: #484c9d;
+			height: 35px;
+			width: 1225px;
+			font-family: sans-serif;
+			color: white;
+			font-size: 31px;
+			padding-left: 10px;
 		}
 	</style>
 </head>
 <body>
+	<div class="barraSup">
+		Solutio!
+	</div>
 	<div class="container">
 	  <p>
 		[thirdCompany]
@@ -44,18 +57,99 @@ USING (VALUES
 	  <p>
 		Hemos recibido como oferta por el siniestro de referencia la suma de: [montoOfrecimiento]
 	  </p>
-	  <br>
 	  <p>
 		Consideramos insuficiente la oferta realizada por vuestra aseguradora y solicitamos se reconsidere el monto ofrecido
-		y
-		proceda a abonar la suma de: [montoReclamado].
+		y proceda a abonar la suma de: [montoReclamado].
 	  </p>
 	  <p>
 		A este monto se le suma lo que determinen los estudios médicos correspondiente a las lesiones (si hay lesiones).
 	  </p>
-	  <br>
 	  <p>
 		Esperamos que juntos podamos trabajar para darle una respuesta eficiente y satisfactoria a los damnificados.
+	  </p>
+	</div>
+</body>
+</html>'),
+	(2,N'Reclamo', N'<html>
+<head>
+	<style>
+		.container{
+			width: 1120;
+			padding: 60px;
+			font-size: 16px;
+			font-family: sans-serif;
+		}
+		
+		.caja{
+			width: 546px;
+			margin-left: 276px;
+			padding: 17px;
+			text-align: left;
+			border: 1px solid #454666;
+		}
+		
+		.barraSup{
+			background-color: #484c9d;
+			height: 35px;
+			width: 1225px;
+			font-family: sans-serif;
+			color: white;
+			font-size: 31px;
+			padding-left: 10px;
+		}
+	</style>
+</head>
+<body>
+	<div class="barraSup">
+		Solutio!
+	</div>
+	<div class="container">
+	  <p>
+		[thirdCompany]
+	  </p>
+	  <p>
+		S__________/__________D
+	  </p>
+	  <p class="caja">
+		Fecha de siniestro: [sinisterDate]
+		<br>
+		El dominio de tu asegurado es: [thirdVehicleDomain]
+		<br>
+		El número de siniestro en tu compañía es: [sinisterNumber]
+	  </p>
+	  <br>
+	  <br>
+	  <p>
+		Felicitaciones, recibiste un reclamo de SOLUTIO! Esperamos que juntos podamos trabajar para darle una respuesta eficiente y satisfactoria a quienes te detallamos a continuación:
+	  </p>
+	  <p>
+		<b>DAMNIFICADOS:</b><br><br>
+		[nombrePersona1] - [dniPersona1] - [enCaracterDePersona1] <br>
+		[nombrePersona2] - [dniPersona2] - [enCaracterDePersona2] <br>
+		[nombrePersona3] - [dniPersona3] - [enCaracterDePersona3] <br>
+	  </p>
+	  <p>
+		<b>BIEN DAÑADO:</b><br><br>
+		[dominioVehiculo1] - [tipoDeVehiculo1] <br>
+		[dominioVehiculo2] - [tipoDeVehiculo2] <br>
+		[dominioVehiculo3] - [tipoDeVehiculo3] <br>
+	  </p>
+	  <p>
+		<b>¿CUÁL ES LA JUSTA INDEMNIZACION?</b><br><br>
+		DAÑO EMERGENTE: [montoAReclamar] <br>
+		LESIONES: (si hay lesiones) Monto justo que determinen los estudios médicos realizados <br>
+		Todo lo arriba mencionado con más los intereses legales desde la fecha de producción del siniestro hasta su real y efectivo pago, gastos y honorarios profesionales seria lo justo.
+	  </p>
+	  <p>
+		<b>¿QUE HECHOS FUNDAMENTAN ESTO?</b><br><br>
+		Direccion del siniestro: [DireccionDelSiniestro]	 <br>		
+		Entre Calles: [entreCalles] <br>
+		Localidad: [Localidad] 		 <br>	
+		Provincia: [Provincia]		 <br>	
+		País: [País]  <br>
+		Fecha de siniestro: [FechaDeSiniestro]		 <br>	
+		Hora: [HoraSiniestro] <br>
+		Relato del hecho Relato del hecho: [relato] <br>
 	  </p>
 	</div>
 </body>
